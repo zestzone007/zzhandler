6690a57bc4206e99f0688a1d7067129fb42a005f
zest <zestzone007@163.com>
2020-10-28 22:40:38 +0800

    init commit

---
+++ app/src/main/java/com/zestzone/practice/zzhandler/MainActivity.kt
@@ -0,0 +1,11 @@
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.zestzone.practice.zzhandler.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ app/src/main/java/com/zestzone/practice/zzhandler/MessageBean.java
@@ -0,0 +1,9 @@
+package com.zestzone.practice.zzhandler;
+
+public class MessageBean {
+    public String from;
+    public String to;
+    public String type;
+    public String content;
+    public long sendTime;
+}
